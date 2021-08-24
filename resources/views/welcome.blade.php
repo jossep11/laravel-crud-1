33b77204684f10535d90075942a06727e8107f6a
JossepP99 <jossepparedes1@gmail.com>
2021-08-23 21:35:17 -0400

    test deploy 0.0.1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -512,9 +512,12 @@
                 <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                     Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                 </div>
+                
+                <a href="/articulos" class="underline text-gray-900 dark:text-white">test</a>
             </div>
         </div>
     </div>
+   
 </body>
 
 </html>
